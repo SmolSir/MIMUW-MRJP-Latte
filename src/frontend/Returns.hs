32d75000ac336404c44ebefda4d29e05982c5e08
SmolSir <b.smolarczyk@student.uw.edu.pl>
2024-01-19 20:02:42 +0100

    frontend returns checks

--- src/frontend/Returns.hs
+++ src/frontend/Returns.hs
@@ -8,29 +8,66 @@ import Control.Monad.Reader
 import Control.Monad.Trans.Except
 
 
+---------------------
+-- check functions --
+---------------------
+returnCheck :: Stmt -> TCMonad Bool
+returnCheck (BStmt (Block statementList)) = foldM findReturn False statementList
+    where
+        findReturn accumulator statement = (||) accumulator <$> returnCheck statement
+
+returnCheck (Ret _) = return True
+
+returnCheck VRet = return True
+
+returnCheck (Cond  ELitTrue statement) = returnCheck statement
+
+returnCheck (CondElse ELitTrue  statementTrue _) = returnCheck statementTrue
+
+returnCheck (CondElse ELitFalse _ statementFalse) = returnCheck statementFalse
+
+returnCheck (CondElse _ statementTrue statementFalse) = do
+    returnStatementTrue  <- returnCheck statementTrue
+    returnStatementFalse <- returnCheck statementFalse
+    return (returnStatementTrue && returnStatementFalse)
+
+returnCheck (While ELitTrue statement) = returnCheck statement
+
+returnCheck (SExp (EApp (Ident "error") _)) = return True
+
+returnCheck _ = return False
+
+classMemberReturnCheck :: ClsMem -> TCMonad Bool
+classMemberReturnCheck (Attr _ (Ident _)) = return False
+
+classMemberReturnCheck (Meth Void (Ident _) _ _) = return True
+
+classMemberReturnCheck (Meth _ (Ident identifier) _ block) = do
+    isValid <- returnCheck (BStmt block)
+    if isValid
+        then return False
+        else throwTCMonad $ "No return value in method `" ++ identifier ++ "`\n"
+
+topDefReturnCheck :: TopDef -> TCMonad ()
+topDefReturnCheck (FnDef Void (Ident _         ) _ _    ) = return ()
+
+topDefReturnCheck (FnDef _    (Ident identifier) _ block) = do
+    isValid <- returnCheck (BStmt block)
+    if isValid
+        then return ()
+        else throwTCMonad $ "No return value in function `" ++ identifier ++ "`\n"
+
+topDefReturnCheck (ClsDef (Ident identifier) _ classMemberList) =
+    mapM_ classMemberReturnCheck classMemberList `throwAdditionalMessage` errorMessage
+    where
+        errorMessage error =
+            error ++
+            "in the following class:\n" ++
+            identifier ++
+            "\n"
+
+-------------------
+-- run functions --
+-------------------
 runReturnsCheck :: [TopDef] -> TCMonad ()
 runReturnsCheck = mapM_ topDefReturnCheck
-    where
-        returnCheck :: Stmt -> TCMonad Bool
-        returnCheck (Ret _) = return True
-        returnCheck VRet    = return True
-        returnCheck (CondElse ELitTrue  statementTrue _)              = returnCheck statementTrue
-        returnCheck (CondElse ELitFalse _             statementFalse) = returnCheck statementFalse
-        returnCheck (CondElse _         statementTrue statementFalse) = do
-            returnStatementTrue  <- returnCheck statementTrue
-            returnStatementFalse <- returnCheck statementFalse
-            return (returnStatementTrue && returnStatementFalse)
-        returnCheck (Cond  ELitTrue statement) = returnCheck statement
-        returnCheck (While ELitTrue statement) = returnCheck statement
-        returnCheck (BStmt (Block statementList)) = foldM findReturn False statementList
-            where
-                findReturn accumulator statement = (||) accumulator <$> returnCheck statement
-        returnCheck _ = return False
-
-        topDefReturnCheck :: TopDef -> TCMonad ()
-        topDefReturnCheck (FnDef Void (Ident _         ) _ _    ) = return ()
-        topDefReturnCheck (FnDef _    (Ident identifier) _ block) = do
-            isValid <- returnCheck (BStmt block)
-            if isValid
-                then return ()
-                else throwTCMonad $ "No return value in function `" ++ identifier ++ "`\n"
